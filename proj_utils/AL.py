2203270c587003a293b06fd8d3f8b34c47300456
daniel <daniel.segal3@mail.huji.ac.il>
2021-03-12 17:27:20 +0200

    Update AL.py

--- proj_utils/AL.py
+++ proj_utils/AL.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader, Dataset, TensorDataset
 import numpy as np
 import util.misc as utils
 
-from torch.utils.data import DataLoader, DistributedSampler
+from torch.utils.data import DataLoader, DistributedSampler, Subset, ConcatDataset
 
 from tqdm import tqdm
 
@@ -22,6 +22,7 @@ class ActiveLearner:
         self.batch_size = batch_size
         # a functions that trains the model for one epoch
         self.model_train = model_train
+        self.device = device
 
         self.history = {}
 
@@ -31,49 +32,71 @@ class ActiveLearner:
         self.query_size = query_size
         self.epochs_per_query = epochs_per_query
 
-        def init_ds(self, dataset, dataset_size):
-            idx = np.random.choice(range(0, len(dataset)), size=dataset_size)
-            return torch.utils.data.Subset(dataset, idx)
+    def init_ds(self, dataset, dataset_size):
+        idx = np.random.choice(range(0, len(dataset)), size=dataset_size)
+        return Subset(dataset, idx)
 
-        def make_val_loader(self, dataset_val):
-            if self.distributed:
-                sampler_val = DistributedSampler(dataset_val, shuffle=False)
+    def make_val_loader(self, dataset_val):
+        if self.distributed:
+            sampler_val = DistributedSampler(dataset_val, shuffle=False)
+        else:
+            sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+
+        if self.model_name == "DETR":
+            data_loader_val = DataLoader(dataset_val, self.batch_size, sampler=sampler_val,
+                                         drop_last=False, collate_fn=utils.collate_fn, num_workers=self.num_workers)
+        return data_loader_val
+
+    def make_train_loader(self, dataset_train):
+        if self.distributed:
+            sampler_train = DistributedSampler(dataset_train)
+        else:
+            sampler_train = torch.utils.data.RandomSampler(dataset_train)
+
+        batch_sampler_train = torch.utils.data.BatchSampler(
+            sampler_train, self.batch_size, drop_last=True)
+
+        data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
+                                       collate_fn=utils.collate_fn, num_workers=num_workers)
+        return data_loader_train
+
+    def query_oracle(self, idx):
+        return Subset(self.dataset_train, idx)
+
+
+    def train_al(self):
+        # train the model with active learning
+        # TODO: add things to history
+
+        loader_val = self.make_val_loader(self.dataset_val)
+
+        # train on the current labeled dataset
+
+        # AL loop
+        n_iters = len(self.dataset_train) // self.query_size
+        for i in range(n_iters):
+            # TODO - change for queries per epoch changes > 1, for DETR this will train for one epoch
+            # train on the current dataset
+            if i == 0:
+                dataset_sub = self.init_ds(self.dataset_train, self.initial_size)
             else:
-                sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+                # query_oracle and and expand the current training set
+                idx = self.al_method(self.model, self.dataset_train)
+                new_samples = self.query_oracle(idx)
+                dataset_sub = ConcatDataset([dataset_sub, new_samples])
 
-            if model_name == "DETR":
-                data_loader_val = DataLoader(dataset_val, self.batch_size, sampler=sampler_val,
-                                             drop_last=False, collate_fn=utils.collate_fn, num_workers=self.num_workers)
-            return data_loader_val
+            loader_subtrain = self.make_train_loader(dataset_sub)
 
-        def make_train_loader(self, dataset_train):
-            if self.distributed:
-                sampler_train = DistributedSampler(dataset_train)
-            else:
-                sampler_train = torch.utils.data.RandomSampler(dataset_train)
+            self.model_train(self.model, self.criterion, loader_subtrain, self.optimizer, self.device, epoch=i)
+            print("Trained on: {}/{}".format(len(dataset_sub, len(self.dataset_train))))
 
-            batch_sampler_train = torch.utils.data.BatchSampler(
-                sampler_train, self.batch_size, drop_last=True)
+        print("[AL]: done")
 
-            data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
-                                           collate_fn=utils.collate_fn, num_workers=num_workers)
 
-            return data_loader_train
 
-        def train_al(self):
-            # train the model with active learning
 
-            dataset_sub = self.init_ds(self.dataset_train, self.initial_size)
-            loader_subtrain = make_train_loader(dataset_sub)
-            loader_val = make_val_loader(self.dataset_val)
 
-            # train on the current labeled dataset
 
-            # AL loop
-            num_iters = len(dataset_train) / query_size
-            for i in tqdm(range(num_iters)):
-                # TODO - change if queries per epoch changes
-                self.model_train(model, criterion, loader_subtrain, optimizer, device, epoch=i)
 
 
 
--- proj_utils/cls_ops.py
+++ proj_utils/cls_ops.py
@@ -9,109 +9,119 @@ import pandas as pd
 import numpy as np
 import pickle
 
+class ClsOperator:
+    def __init__(self, model):
+        self.model = model
+        self.debug = True
 
-CLASSES = [
-    'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
-    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
-    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
-    'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
-    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
-    'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
-    'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
-    'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
-    'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
-    'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
-    'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
-    'toothbrush'
-]
+        self.cls_losses = torch.zeros(len(self.CLASSES), 4)
 
+        self.CLASSES = [
+            'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+            'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+            'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+            'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
+            'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+            'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+            'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
+            'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+            'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+            'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
+            'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+            'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
+            'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+            'toothbrush'
+        ]
 
-def cls_loss(outputs, targets, criterion, cls_losses, weights=None):
-    """ 
+    @torch.no_grad()
+    def get_cls_loss(self, outputs, targets, criterion, cls_losses, weights=None):
+        """
 
-    """
-    # TODO: make sure we are not backpropagating from here
-    # (probably from the function that will call this(with no grad)
+        """
+        # TODO: make sure we are not backpropagating from here
+        # (probably from the function that will call this(with no grad)
 
-    outputs_without_aux = {k: v for k, v in outputs.items() if k != 'aux_outputs'}
-    indices = criterion.matcher(outputs_without_aux, targets)
+        outputs_without_aux = {k: v for k, v in outputs.items() if k != 'aux_outputs'}
+        indices = criterion.matcher(outputs_without_aux, targets)
 
-    src_idx = criterion._get_src_permutation_idx(indices)
+        src_idx = criterion._get_src_permutation_idx(indices)
 
-    # order the labels by the indices
-    target_classes = torch.cat([t["labels"][J] for t, (_, J) in zip(targets, indices)]) # BOXES
-    src_logits = outputs['pred_logits'][src_idx] # (BOXES) X C 
+        # order the labels by the indices
+        target_classes = torch.cat([t["labels"][J] for t, (_, J) in zip(targets, indices)]) # BOXES
+        src_logits = outputs['pred_logits'][src_idx] # (BOXES) X C
 
-    # order the bounding boxes by the indices
-    target_boxes = torch.cat([t['boxes'][i] for t, (_, i) in zip(targets, indices)], dim=0) # BOXES
-    src_boxes = outputs['pred_boxes'][src_idx] # BOXES
+        # order the bounding boxes by the indices
+        target_boxes = torch.cat([t['boxes'][i] for t, (_, i) in zip(targets, indices)], dim=0) # BOXES
+        src_boxes = outputs['pred_boxes'][src_idx] # BOXES
 
-    classes = torch.unique(target_classes)
-    for cls in classes:
-        idx = torch.where(target_classes == cls)[0]
+        classes = torch.unique(target_classes)
+        for cls in classes:
+            idx = torch.where(target_classes == cls)[0]
 
-        loss_ce = F.cross_entropy(src_logits[idx], target_classes[idx], reduction='sum')
+            loss_ce = F.cross_entropy(src_logits[idx], target_classes[idx], reduction='sum')
 
-        loss_bbox = F.l1_loss(src_boxes[idx], target_boxes[idx], reduction='sum')
+            loss_bbox = F.l1_loss(src_boxes[idx], target_boxes[idx], reduction='sum')
 
-        loss_giou = (1 - torch.diag(box_ops.generalized_box_iou(
-            box_ops.box_cxcywh_to_xyxy(src_boxes[idx]),
-            box_ops.box_cxcywh_to_xyxy(target_boxes[idx])))).sum()
+            loss_giou = (1 - torch.diag(box_ops.generalized_box_iou(
+                box_ops.box_cxcywh_to_xyxy(src_boxes[idx]),
+                box_ops.box_cxcywh_to_xyxy(target_boxes[idx])))).sum()
 
-        losses = torch.tensor([len(idx), loss_ce, loss_bbox, loss_giou])
-        cls_losses[cls] += losses
+            losses = torch.tensor([len(idx), loss_ce, loss_bbox, loss_giou])
+            cls_losses[cls] += losses
 
-    return cls_losses
-        
-def create_csv(cls_losses, csv_file="cls_losses.csv"):
-    df = pd.DataFrame(cls_losses.numpy())
+        return cls_losses
 
-    col_names = ["count", "loss_ce", "loss_bbox", "loss_giou", "weighted_loss"]
-    mapping = {i: col_names[i] for i, _ in enumerate(col_names)}
+    def get_cls_weights(self):
+        # TODO: add dirichlet distribution on the weights
+        weighted_losses = self.cls_losses[:, -1]
+        cls_weights = weighted_losses / weighted_losses.sum()
+        if self.debug:
+            print("-"*20)
+            print("Class Weights: ", cls_weights)
+            print("-"*20)
+        return cls_weights
 
-    df = df.rename(columns=mapping)
-    df["cls_name"] = CLASSES
+    def create_csv(self, cls_losses, csv_file="cls_losses.csv"):
+        df = pd.DataFrame(cls_losses.numpy())
 
-    df = df.reindex(columns=["cls_name"] + col_names)
-    df.to_csv(csv_file)
-    return df
+        col_names = ["count", "loss_ce", "loss_bbox", "loss_giou", "weighted_loss"]
+        mapping = {i: col_names[i] for i, _ in enumerate(col_names)}
 
+        df = df.rename(columns=mapping)
+        df["cls_name"] = self.CLASSES
 
-@torch.no_grad()
-def cls_eval(model, criterion, data_loader, base_ds, device, output_dir):
-    # evaluates the model per class
-    cls_losses = torch.zeros(len(CLASSES), 4)
+        df = df.reindex(columns=["cls_name"] + col_names)
+        df.to_csv(csv_file)
+        return df
 
+    @torch.no_grad()
+    def cls_eval(self, criterion, data_loader, base_ds, device, output_dir):
+        # evaluates the model per class
 
+        self.model.eval()
 
-    model.eval()
-
-    for i ,(samples, targets) in tqdm(enumerate(data_loader), total=len(data_loader)):
-        samples = samples.to(device)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        outputs = model(samples)
-        cls_loss(outputs, targets, criterion, cls_losses)
-
-
-    # loss_ce, loss_bbox, loss_giou
-    loss_weights = torch.tensor([1, 5, 2]).type(cls_losses.type())
-    eps = torch.tensor(1e-8).type(cls_losses.type())
-
-    weighted_loss = torch.matmul(cls_losses[:, 1: 1 + len(loss_weights)], loss_weights) / (cls_losses[:, 0] + eps)
-
-    cls_losses = torch.cat((cls_losses, weighted_loss.unsqueeze(0).T), dim=1)
-
-    create_csv(cls_losses)
-    #np.save("cls_losses.mat", cls_losses.numpy(), pickle=True)
-
-
-    print(cls_losses)
-    print("finished evaluating class hardness")
+        for i ,(samples, targets) in tqdm(enumerate(data_loader), total=len(data_loader)):
+            samples = samples.to(device)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            outputs = self.model(samples)
+            self.get_cls_loss(outputs, targets, criterion, self.cls_losses)
 
 
-    return cls_losses
+        # loss_ce, loss_bbox, loss_giou
+        loss_weights = torch.tensor([1, 5, 2]).type(self.cls_losses.type())
+        eps = torch.tensor(1e-8).type(self.cls_losses.type())
+
+        weighted_loss = torch.matmul(self.cls_losses[:, 1: 1 + len(loss_weights)], loss_weights) / (self.cls_losses[:, 0] + eps)
+
+        self.cls_losses = torch.cat((self.cls_losses, weighted_loss.unsqueeze(0).T), dim=1)
+
+        self.create_csv(self.cls_losses)
+        #np.save("cls_losses.mat", cls_losses.numpy(), pickle=True)
+
+        print(self.cls_losses)
+        print("finished evaluating class hardness")
+
+        return self.cls_losses
 
 
 
